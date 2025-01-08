bbbc97436e8fd7f2756bab24bac253d23327b351
unknown <hd00rc@gmail.com>
2025-01-08 16:02:42 -0400

    html home

---
+++ wp-content/themes/theme-ramcon/page-home.php
@@ -0,0 +1,136 @@
+<?php get_header(); ?>
+
+<div class="home-page">
+
+  <section id="hero">
+    <div class="container">
+      <h1>TRADITIONAL COOKING WITH A MODERN TWIST</h1>
+      <a href="#">Contact Us</a>
+    </div>
+  </section>
+
+  <section id="contact-info">
+    <div class="container">
+      <div class="grid-items">
+
+        <div class="c-item">
+          <span class="title">Address</span>
+          <span class="description">2605 Scheufront Drive. New York, NY</span>
+        </div>
+        
+        <div class="c-item">
+          <span class="title">OPENING HOURS</span>
+          <span class="description">Mon - Sun</span>
+          <span class="description">8am - 9pm</span>
+        </div>
+
+        <div class="c-item">
+          <span class="title">PHONES</span>
+          <span class="description">+1 234 56789</span>
+          <span class="description">+1 234 56789</span>
+        </div>
+
+        <div class="c-item">
+          <span class="title">EMAILS</span>
+          <span class="description">info@link.com</span>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section id="about">
+    <div class="container">
+      <div class="grid-cols">
+        <div class="col col-left">
+          <h3>The Best Meals</h3>
+          <h2>THE PERFECT PLACE TO ENJOY THE LIFE AND FOOD</h2>
+          <img src="" alt="">
+        </div>
+        <div class="col col-right">
+          <img src="" alt="">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nobis architecto quas distinctio reiciendis ipsum facere possimus, illo harum corporis commodi ea, dignissimos necessitatibus asperiores nisi laboriosam perspiciatis deleniti neque minima deserunt repudiandae repellat ab beatae! Quaerat veniam sequi amet fugit est hic facere quia natus cum non quae sint eum, debitis ipsam deleniti corporis provident perferendis quos voluptas eveniet. Sed voluptas facilis iste aliquam pariatur minima adipisci ea, vitae dolores nisi similique amet magni doloremque, cumque recusandae ratione beatae deleniti neque. Aspernatur sit exercitationem ea eius perferendis, dolor doloremque eligendi, reiciendis necessitatibus mollitia ex dicta alias adipisci aut consequuntur.</p>
+        </div>
+        <a href="#">More About Us</a>
+      </div>
+    </div>
+  </section>
+
+  <section id="menu">
+    <div class="container">
+      <div class="grid-items">
+
+        <div class="c-item">
+          <img src="" alt="">
+          <div class="c-content">
+            <h3>MUSHROOM & ONION QUESADILLA</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequuntur, maiores dignissimos qui alias obcaecati quae magnam facere numquam fugit.</p>
+            <span class="price">$16</span>
+          </div>
+        </div>
+
+      </div>
+
+      <a href="#">Explore Our Menu</a>
+    </div>
+  </section>
+
+  <section id="gallery">
+    <div class="container">
+      <div class="grid-items">
+
+      <div class="c-item">
+          <img src="" alt="">
+          <div class="c-content">
+            <h4>Name Image</h4>
+          </div>
+        </div>
+
+        <div class="c-item">
+          <img src="" alt="">
+          <div class="c-content">
+            <h4>Name Image</h4>
+          </div>
+        </div>
+
+        <div class="c-item">
+          <img src="" alt="">
+          <div class="c-content">
+            <h4>Name Image</h4>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section id="banners">
+    <div class="container">
+      <div class="gris-cols">
+        
+        <div class="col">
+
+        </div>
+        
+        <div class="col">
+          <h4>Why Choose Us</h4>
+          <h3>#1 LOCATION FOR FIRST-CLASS BBQ</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati perspiciatis numquam quos corporis ratione, incidunt suscipit laboriosam expedita. Est dolorem iure praesentium culpa ratione officia?</p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section id="suscribe">
+    <h3>Newsletter Signup</h3>
+    <h2>JOIN OUR NEWSLETTER LIST TO GET ALL THE LATEST OFFERS AND DISCOUNTS</h2>
+    <form action="">
+      <input type="email" name="email" id="suscribe-email">
+      <input type="submit" value="Subscribe">
+    </form>
+  </section>
+
+</div>
+
+<?php get_footer(); ?>
